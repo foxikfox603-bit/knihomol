f1e554010122bc6391d28e6f9c4472a8887be5a8
foxikfox603-bit <foxikfox603@gmail.com>
2026-09-18 19:35:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  // Prázdný fetch listener stačí k tomu, aby PWA splnilo podmínku pro instalaci
-  event.respondWith(fetch(event.request).catch(() => {
-    return caches.match(event.request);
-  }));
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
